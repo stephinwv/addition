10e9b746d71ac290012105521922bbb30116ee0a
stephinwv <thekingdomseeker@aol.com>
2017-06-29 13:49:21 -0400

    Add files via upload

---
+++ addition.rb
@@ -0,0 +1,11 @@
+puts "Please enter number 1 : "
+# input in console
+num_1 = gets.chomp.to_i
+print "Please enter number 2 : "
+num_2 = gets.chomp.to_i
+num_3 = num_1 + num_2 
+print "Answer : " , (num_1 + num_2), "\n"
+
+
+
+
